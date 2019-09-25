01b285ec1f676f218f96dc9523a672e6a395e8be
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-09-25 00:49:27 -0700

    Adding to test suite

--- test/quirk/set-capitalize-fragments.js
+++ test/quirk/set-capitalize-fragments.js
@@ -8,6 +8,27 @@ describe("set capitalize fragments", function () {
         expect(testSub.toPlain("hello my name is karkat")).to.equal("Hello my name is karkat");
     });
 
+    it("should capitalize sentence fragments when used in conjunction with uppercase", function () {
+        let testSub = new Quirk();
+        testSub.setCapitalizeFragments(true);
+        testSub.setSentenceCase("uppercase");
+        expect(testSub.toPlain("HELLO MY NAME IS KARKAT")).to.equal("Hello my name is karkat");
+    });
+
+    it("should capitalize sentence fragments when used in conjunction with alternatingcase", function () {
+        let testSub = new Quirk();
+        testSub.setCapitalizeFragments(true);
+        testSub.setSentenceCase("alternatingcaps");
+        expect(testSub.toPlain("hElLO")).to.equal("Hello");
+    });
+
+
+    it("should capitalize sentence fragments even when that doesn't make sense", function () {
+        let testSub = new Quirk();
+        testSub.setCapitalizeFragments(true);
+        expect(testSub.toPlain("Hello my name is karkat")).to.equal("Hello my name is karkat");
+    });
+
     it("should throw an error when given an invalid type", function () {
         const badFn = () => {
             let testSub = new Quirk();
