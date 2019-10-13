58f8d582d86773c331e3876986cbfb066a12d094
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-10-13 16:50:33 -0700

    Adding linkedlist class

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Quirk = require("./quirk");
+/* const Quirk = require("./quirk");
 let example = new Quirk();
 example.setPrefix(">>");
 example.setSuffix("<<");
@@ -10,4 +10,4 @@ console.log(example.toQuirk("Check this stuff out! It even works for multiple se
 //Outputs: >>check thii2 2tuff out!<< >>iit even work2 for multiiple 2entence2.<<
 
 console.log(example.toPlain(">>check thii2 2tuff out!<< >>iit even work2 for multiiple 2entence2.<<"));
-//Outputs: Check this stuff out! It even works for multiple sentences.
+//Outputs: Check this stuff out! It even works for multiple sentences. */
--- slinkedList.js
+++ slinkedList.js
@@ -1,4 +1,116 @@
-class sLinkedList {
-
-    
+//Node class
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        //figure out where we determine the nodetype
+        //this.nodeType = nodeType;
+    }
 }
+
+//LinkedList
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+        this.next = null;
+    }
+
+    // functions to be implemented 
+    add(element) {
+        let newNode = new Node(element);
+        let currentNode;
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            currentNode = this.head;
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        this.size++;
+    }
+
+    insertAt(index, element) {
+        //Returns FALSE on a failed insertion
+        //Returns TRUE on a successfull insertion
+        if (index > this.size - 1 || index < 0) {
+            return false; //don't insert if the index is nonsense
+        }
+        //create a new node
+        let newNode = new Node(element);
+
+        if (index === 0) { //special case: inserting at head
+            newNode.next = this.head;
+            this.head = newNode;
+        } else { //otherwise we have to find the right place in the chain to insert!
+            let currentNode = this.head;
+            let previousNode = null;
+            let currentIndex = 0;
+            while (currentIndex < index) {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+                currentIndex++;
+            }
+            //the new node is going to be AT the index we provided, so:
+            newNode.next = currentNode;
+            previousNode.next = newNode;
+        }
+        this.size++;
+        return true;
+    }
+
+    removeFrom(index) { //deletes a node at a particular location
+        //Returns FALSE on a failed insertion
+        //Returns TRUE on a successfull insertion
+        if (index > this.size - 1 || index < 0) {
+            return false; //don't insert if the index is nonsense
+        }
+
+        if (index === 0) { //special case: removing from head
+            let newHead = this.head.next;
+            this.head = newHead;
+        } else { //otherwise we have to find the right place in the chain to renove!
+            let currentNode = this.head;
+            let previousNode = null;
+            let currentIndex = 0;
+            while (currentIndex < index) {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+                currentIndex++;
+            }
+            //we are removing FROM the index we provided, so:
+            previousNode.next = currentNode.next;
+        }
+        this.size--;
+        return true;
+    }
+
+    // Helper Methods 
+    //GETTER for the length
+    get length() {
+        return this.size;
+    }
+
+    //Quick test if the list is empty
+    isEmpty() {
+        return (this.length > 0 ? false : true);
+    }
+
+    printList() {
+        if (this.isEmpty()) {
+            return console.log("The current list is empty.");
+        }
+        console.log(`Printing your list of size ${this.size}`);
+        let currentNode = this.head;
+
+        do {
+            console.log(currentNode.value);
+            currentNode = currentNode.next;
+        } while (currentNode);
+    }
+
+}
+
+module.exports = LinkedList;
